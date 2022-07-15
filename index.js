7456ebe8eda902c9ecc843b3de3494fe76b8f1b8
Prince <PrincePR8920@gmail.com>
2022-07-15 14:18:00 +0530

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
+ 
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  let url = req.body.url;
+  let regex = /(^[https]+\:\/\/[www]+\.)(\w+)/gi
+  let short = url.replace(regex,"$2").slice(0,4)
+  console.log(`The short url is ${short}`)
+  app.locals.shortURL = short
+  app.locals.org = url
+  res.json({ "original_url":url,"short_url":short});
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shorten?", function (req, res) {
+  let params = req.params.shorten 
+  if(params === app.locals.shortURL){
+    res.redirect(app.locals.org)
+  }else{
+    res.send("something went wrong")
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
