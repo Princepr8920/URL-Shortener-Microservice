aaa9f4c6dca7d40f92f5e3fc3845bfb9756ef5de
Prince <PrincePR8920@gmail.com>
2022-08-13 15:40:27 +0530

    first commit

--- index.js
+++ index.js
@@ -1,70 +1,63 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const shortid = require("shortid")
-const mongodb = require("mongodb")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
+const body = require("body-parser");
 const port = process.env.PORT || 1000;
-const logger = require("morgan")
-require('dotenv').config();
+const logger = require("morgan");
+const shortid = require("shortid");
+app.use(logger("dev"));
+app.use(body.urlencoded({ extended: false }));
 
-app.use(logger("dev"))
-
-app.use(bodyParser.urlencoded({ extended: false }));  
-
-mongoose.connect(process.env.MY_DB,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
-  console.log("Database is connected 🧠")
-})
+mongoose.connect(process.env.MY_DB, 
+  () => {
+    console.log("Database is connected 🧠");
+  }
+);
 
 app.use(cors());
-app.use(bodyParser.json())
-app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
- 
+
 let urlSchema = mongoose.Schema({
-  short_url:String,
-  original_url:String
-})
+  short_url: String,
+  original_url: String,
+});
 
 let urlModel = mongoose.model("myUrl", urlSchema);
 
-app.post('/api/shorturl', function(req, res) {
-  let url =  req.body.url;
-  let newUrl = shortid.generate()
-  let isValidUrl = /(^[https\:\/\/]+[www\.]*)\w+[\.]+(?=com+$|org+$|in+$)/gi;
-  if(!isValidUrl.test(url)){
-      res.json({ error: 'invalid url' });
-  }else{
-  let shorted = new urlModel({
-    short_url: newUrl,
-    original_url:url
-  })
+app.post("/api/shorturl", function (req, res) {
+  let url = req.body.url;
+  let newShortUrl = shortid.generate();
+  let isValidUrl = /(^[http\:\/\/]+[www\.]*)/gi;
 
-   shorted.save((err,url)=>{
-    if (err) return err;
-     res.json(url)
-   })}
-
- 
+  if (isValidUrl.test(url)) {
+    let shorted = new urlModel({
+      short_url: newShortUrl,
+      original_url: url,
+    });
+    shorted.save((err, url) => {
+      if (err) return err;
+      res.json(url);
+    });
+  } else {
+    res.json({ error: "invalid url" });
+  }
 });
 
-app.get("/api/shorturl/:mini?",(req,res)=>{
-let param = req.params.mini;
+app.get("/api/shorturl/:mini", (req, res) => {
+  let param = req.params.mini;
+  urlModel.findOne({ short_url: param }, (err, url) => {
+    if (err) return err; 
+    res.redirect(url.original_url);
+  });
+});
 
-urlModel.findOne({short_url:param},(err,url)=>{
-  if(err) return err;
-  console.log(url)
-  res.redirect(url.original_url)
-})
-})
-
-
-
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port} ✔`);
 });
